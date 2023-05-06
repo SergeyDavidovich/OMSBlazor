ebaf8d798ed9154b5f89e5d58977e9dbbcebce78
Bekzod Allaev <allaev_@outlook.com>
2023-05-06 08:22:47 +0500

    Add migration that adds feature tables

--- src/OMSBlazor.EntityFrameworkCore/OMSBlazorIdentity/20230506032059_AddAbpFeatureTables.Designer.cs
+++ src/OMSBlazor.EntityFrameworkCore/OMSBlazorIdentity/20230506032059_AddAbpFeatureTables.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OMSBlazor.EntityFrameworkCore;
 using Volo.Abp.EntityFrameworkCore;
@@ -12,9 +13,11 @@ using Volo.Abp.MultiTenancy;
 namespace OMSBlazor.OMSBlazorIdentity
 {
     [DbContext(typeof(OMSBlazorIdentityDbContext))]
-    partial class OMSBlazorIdentityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230506032059_AddAbpFeatureTables")]
+    partial class AddAbpFeatureTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
