591a3a3d8f32fff154f11df8d450d8f269ba6910
Bekzod Allaev <allaev_@outlook.com>
2023-06-01 01:34:17 +0500

    Add Employee aggregate

---
+++ src/OMSBlazor.Domain/Northwind/EmployeeAggregate/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities;
+
+namespace OMSBlazor.Northwind.EmployeeAggregate
+{
+    public class Employee : AggregateRoot<Guid>
+    {
+        public Employee(Guid id)
+            :base(id)
+        {
+            
+        }
+    }
+}
