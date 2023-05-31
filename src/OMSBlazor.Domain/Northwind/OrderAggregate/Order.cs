4cf1a70049fa50baa1d1aee762840c5d83a07363
Bekzod Allaev <allaev_@outlook.com>
2023-06-01 01:34:26 +0500

    Add Order aggregate

---
+++ src/OMSBlazor.Domain/Northwind/OrderAggregate/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities;
+
+namespace OMSBlazor.Northwind.OrderAggregate
+{
+    public class Order : AggregateRoot<Guid>
+    {
+        private Order() { }
+
+        public Order(Guid id) :
+            base(id)
+        {
+
+        }
+    }
+}
---
+++ src/OMSBlazor.Domain/Northwind/OrderAggregate/OrderDetail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Values;
+
+namespace OMSBlazor.Northwind.OrderAggregate
+{
+    public class OrderDetail : ValueObject
+    {
+        protected override IEnumerable<object> GetAtomicValues()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
