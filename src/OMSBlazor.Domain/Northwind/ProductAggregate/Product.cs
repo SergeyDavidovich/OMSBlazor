98a4e560bfe046d59edd52e1140fd0fc76ba6699
Bekzod Allaev <allaev_@outlook.com>
2023-06-01 01:34:35 +0500

    Add Product aggregate

---
+++ src/OMSBlazor.Domain/Northwind/ProductAggregate/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities;
+
+namespace OMSBlazor.Northwind.ProductAggregate
+{
+    public class Product : AggregateRoot<Guid>
+    {
+        public Product(Guid id) :
+            base(id)
+        {
+
+        }
+    }
+}
