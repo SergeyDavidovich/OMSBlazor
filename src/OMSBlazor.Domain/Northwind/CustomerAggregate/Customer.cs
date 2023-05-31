2b3532a7f0177f8871b52eb98b6c95edbe6a583a
Bekzod Allaev <allaev_@outlook.com>
2023-06-01 01:36:55 +0500

    Add Customer aggregate

---
+++ src/OMSBlazor.Domain/Northwind/CustomerAggregate/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities;
+
+namespace OMSBlazor.Northwind.CustomerAggregate
+{
+    public class Customer: AggregateRoot<Guid>
+    {
+        public Customer(Guid id):
+            base(id)
+        {
+            
+        }
+    }
+}
