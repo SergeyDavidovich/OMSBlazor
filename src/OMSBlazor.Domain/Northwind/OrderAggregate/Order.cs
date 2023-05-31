a2f6e97e268d9df918a8873040dd87d2f47fbd1a
Bekzod Allaev <allaev_@outlook.com>
2023-06-01 01:57:45 +0500

    Define members of Order

--- src/OMSBlazor.Domain/Northwind/OrderAggregate/Order.cs
+++ src/OMSBlazor.Domain/Northwind/OrderAggregate/Order.cs
@@ -11,10 +11,29 @@ namespace OMSBlazor.Northwind.OrderAggregate
     {
         private Order() { }
 
-        public Order(Guid id) :
+        public Order(
+            Guid id, 
+            Guid employeeId, 
+            Guid customerId) :
             base(id)
         {
-
+            EmployeeId = employeeId;
+            CustomerId = customerId;
         }
+
+        public Guid EmployeeId { get; private set; }
+
+        public Guid CustomerId { get; private set; }
+
+        /// <summary>
+        /// You can treat this as ShipperId. Don't change name of this property it should match with db column name
+        /// </summary>
+        public Guid ShipVia { get; private set; }
+
+        public DateTime OrderDate { get; private set; }
+
+        public DateTime RequiredDate { get; private set; }
+
+        public ShipData ShipData { get; private set; }
     }
 }
