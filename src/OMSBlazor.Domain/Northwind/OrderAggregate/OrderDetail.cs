286b4ace199310601768bab379ce79fcabc70084
Bekzod Allaev <allaev_@outlook.com>
2023-06-01 01:48:36 +0500

    Inherit OrderDetail from Entity and define strcture of the OrderDetail

--- src/OMSBlazor.Domain/Northwind/OrderAggregate/OrderDetail.cs
+++ src/OMSBlazor.Domain/Northwind/OrderAggregate/OrderDetail.cs
@@ -3,15 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities;
 using Volo.Abp.Domain.Values;
 
 namespace OMSBlazor.Northwind.OrderAggregate
 {
-    public class OrderDetail : ValueObject
+    public class OrderDetail : Entity
     {
-        protected override IEnumerable<object> GetAtomicValues()
+        public OrderDetail(
+            Guid orderId, 
+            Guid productId)
         {
-            throw new NotImplementedException();
+            OrderId = orderId;
+            ProductId = productId;
+        }
+
+        public Guid OrderId { get; private set; }
+
+        public Guid ProductId { get; private set; }
+
+        public double UnitPrice { get; internal set; }
+
+        public int Quantity { get; internal set; }
+
+        public float Discount { get; internal set; }
+
+        public override object[] GetKeys()
+        {
+            return new object[] { OrderId, ProductId };
         }
     }
 }
