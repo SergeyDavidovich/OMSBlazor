47472f915174c6a541211f78fc7a8b5e5d41c140
Bekzod Allaev <allaev_@outlook.com>
2023-06-08 00:39:00 +0500

    Add Category entity

---
+++ src/OMSBlazor.Domain/Northwind/ProductAggregate/Category.cs
@@ -0,0 +1,35 @@
+﻿using OMSBlazor.Northwind.ProductAggregate.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities;
+
+namespace OMSBlazor.Northwind.ProductAggregate
+{
+    public class Category : Entity<Guid>
+    {
+        public Category(Guid id, string name)
+            : base(id) 
+        {
+            SetCategoryName(name);
+        }
+
+        public string CategoryName { get; private set; }
+
+        public string? Description { get; set; }
+
+        public string? Picture { get; set; }
+
+        public void SetCategoryName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new EmptyCategoryNameException();
+            }
+
+            CategoryName = name;
+        }
+    }
+}
