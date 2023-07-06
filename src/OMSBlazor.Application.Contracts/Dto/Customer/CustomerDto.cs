97e9a97adc5c638cd8317bfe1d565b106e4b620f
Bekzod Allaev <allaev_@outlook.com>
2023-07-07 00:08:46 +0500

    Add CustomerDto

---
+++ src/OMSBlazor.Application.Contracts/Dto/Customer/CustomerDto.cs
@@ -0,0 +1,22 @@
+﻿using OMSBlazor.Dto.Shared;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OMSBlazor.Dto.Customer
+{
+    public class CustomerDto
+    {
+        public string CompanyName { get; private set; }
+
+        public string ContactName { get; set; }
+
+        public string ContactTitle { get; set; }
+
+        public AddressObjectDto Address { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Fax { get; set; }
+    }
+}
