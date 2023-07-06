a6d2a3bfd2d879767c7afab90a5e2fe2738ec768
Bekzod Allaev <allaev_@outlook.com>
2023-07-07 00:04:17 +0500

    Define members for OrderDto

--- src/OMSBlazor.Application.Contracts/Dto/Order/OrderDto.cs
+++ src/OMSBlazor.Application.Contracts/Dto/Order/OrderDto.cs
@@ -4,7 +4,25 @@ using System.Text;
 
 namespace OMSBlazor.Dto.Order
 {
-    internal class OrderDto
+    public class OrderDto
     {
+        public int EmployeeId { get; set; }
+
+        public int CustomerId { get; set; }
+
+        /// <summary>
+        /// You can treat this as ShipperId. Don't change name of this property it should match with db column name
+        /// </summary>
+        public int ShipVia { get; set; }
+
+        public double Freight { get; }
+
+        public DateTime OrderDate { get; set; }
+
+        public DateTime RequiredDate { get; set; }
+
+        public ShipDataDto? ShipData { get; set; }
+
+        public List<OrderDetailDto> OrderDetails { get; set; }
     }
 }
