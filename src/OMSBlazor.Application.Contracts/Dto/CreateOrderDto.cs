b1f21f204c688a74d680533d79e0c59d1521856b
Bekzod Allaev <allaev_@outlook.com>
2023-07-06 23:51:31 +0500

    Create CreateOrderDto and OrderDetailDto

---
+++ src/OMSBlazor.Application.Contracts/Dto/CreateOrderDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OMSBlazor.Dto
+{
+    public class CreateOrderDto
+    {
+        public CreateOrderDto(int customerId, int employeeId, List<OrderDetailDto> orderDetails)
+        {
+            CustomerId = customerId;
+            EmployeeId = employeeId;
+            OrderDetails = orderDetails;
+        }
+
+        public int CustomerId { get; }
+
+        public int EmployeeId { get; }
+
+        public List<OrderDetailDto> OrderDetails { get; }
+    }
+}
---
+++ src/OMSBlazor.Application.Contracts/Dto/OrderDetailDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OMSBlazor.Dto
+{
+    public class OrderDetailDto
+    {
+        public int ProductId { get; set; }
+
+        public double UnitPrice { get; set; }
+
+        public float Discount { get; set; }
+    }
+}
