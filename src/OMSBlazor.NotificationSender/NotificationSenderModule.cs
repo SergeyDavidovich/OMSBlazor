97488576a862973588efb0193f026a1670488085
Bekzod Allaev <allaev_@outlook.com>
2023-08-06 23:51:50 +0500

    Add signalr client library to blazor project

--- src/OMSBlazor.NotificationSender/NotificationSenderModule.cs
+++ src/OMSBlazor.NotificationSender/NotificationSenderModule.cs
@@ -1,9 +1,7 @@
 using Volo.Abp.Modularity;
-using Volo.Abp.AspNetCore.SignalR;
 
 namespace OMSBlazor.NotificationSender
 {
-    [DependsOn(typeof(AbpAspNetCoreSignalRModule))]
     public class NotificationSenderModules : AbpModule
     {
 
