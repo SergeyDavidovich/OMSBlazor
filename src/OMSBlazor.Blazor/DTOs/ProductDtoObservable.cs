29985b53f69e8c38f662a6d1c57e9d0e0fffc090
Bekzod Allaev <allaev_@outlook.com>
2023-09-18 00:06:45 +0500

    Create ProductDto that implements INotifyPropertyChanged

---
+++ src/OMSBlazor.Blazor/DTOs/ProductDtoObservable.cs
@@ -0,0 +1,31 @@
+﻿using OMSBlazor.Dto.Product;
+using System.ComponentModel;
+
+namespace OMSBlazor.Blazor.DTOs
+{
+    public class ProductDtoObservable : ProductDto, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public override int UnitsInStock 
+        {
+            get 
+            { 
+                return base.UnitsInStock; 
+            }
+            set 
+            { 
+                base.UnitsInStock = value; 
+                NotifyPropertyChanged(nameof(UnitsInStock));
+            }
+        }
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged is not null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
