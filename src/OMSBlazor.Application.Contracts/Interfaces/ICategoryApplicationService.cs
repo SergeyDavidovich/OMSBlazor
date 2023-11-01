da755e35805106cbddf0e9f0614fa41a015f28a7
Bekzod Allaev <allaev_@outlook.com>
2023-11-01 22:49:17 +0200

    Add ICategoryApplicationService

---
+++ src/OMSBlazor.Application.Contracts/Interfaces/ICategoryApplicationService.cs
@@ -0,0 +1,22 @@
+﻿using OMSBlazor.Dto.Category;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+
+namespace OMSBlazor.Interfaces
+{
+    public interface ICategoryApplicationService : IApplicationService
+    {
+        public Task<List<CategoryDto>> GetCategoriesAsync();
+
+        public Task<CategoryDto> GetCategoryAsync(int id);
+
+        public Task DeleteCategoryAsync(int id);
+
+        public Task CreateCategoryAsync(CreateCategoryDto categoryDto);
+
+        public Task UpdateCategoryAsync(int id, UpdateCategoryDto categoryDto);
+    }
+}
