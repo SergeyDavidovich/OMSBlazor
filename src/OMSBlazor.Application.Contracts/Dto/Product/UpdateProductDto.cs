2341f7bc15855f879e0535028f8f876fa91db365
Bekzod Allaev <allaev_@outlook.com>
2023-11-01 22:24:36 +0200

    Add dtos for creating and updating product

---
+++ src/OMSBlazor.Application.Contracts/Dto/Product/UpdateProductDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OMSBlazor.Dto.Product
+{
+    public class UpdateProductDto
+    {
+        public int CategoryId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string QuantityPerUnit { get; set; }
+
+        public double UnitPrice { get; set; }
+
+        public virtual int UnitsInStock { get; set; }
+
+        public int UnitsOnOrder { get; set; }
+
+        public int ReorderLevel { get; set; }
+
+        public bool Discontinued { get; set; }
+    }
+}
