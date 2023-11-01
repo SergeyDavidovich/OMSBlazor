14aa9658ccb4e2a5cd7eeb22e02689128e70b268
Bekzod Allaev <allaev_@outlook.com>
2023-11-01 21:58:01 +0200

    Add crud methods to customer application service

--- src/OMSBlazor.Application.Contracts/Interfaces/ICustomerApplcationService.cs
+++ src/OMSBlazor.Application.Contracts/Interfaces/ICustomerApplcationService.cs
@@ -10,5 +10,13 @@ namespace OMSBlazor.Application.Contracts.Interfaces
     public interface ICustomerApplcationService : IApplicationService
     {
         public Task<List<CustomerDto>> GetCustomersAsync();
+
+        public Task<CustomerDto> GetCustomerAsync(string customerId);
+
+        public Task DeleteCustomerAsync(string customerId);
+
+        public Task CreateCustomerAsync(CreateCustomerDto customerDto);
+
+        public Task UpdateCustomerAsync(UpdateCustomerDto customerDto);
     }
 }
