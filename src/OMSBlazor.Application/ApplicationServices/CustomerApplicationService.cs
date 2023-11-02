f61efc73d60c01d9e0e549cc120b426beaab2f6e
Bekzod Allaev <allaev_@outlook.com>
2023-11-02 23:08:33 +0200

    Implement methods of CustomerApplicationService

--- src/OMSBlazor.Application/ApplicationServices/CustomerApplicationService.cs
+++ src/OMSBlazor.Application/ApplicationServices/CustomerApplicationService.cs
@@ -8,16 +8,47 @@ using System.Threading.Tasks;
 using Volo.Abp.Application.Services;
 using Volo.Abp.Domain.Repositories;
 using OMSBlazor.Northwind.OrderAggregate;
+using OMSBlazor.DomainManagers.Customer;
 
 namespace OMSBlazor.Application.ApplicationServices
 {
     public class CustomerApplicationService : ApplicationService, ICustomerApplcationService
     {
         private readonly IRepository<Customer, string> _customerRepository;
+        private readonly ICustomerManager _customerManager;
 
-        public CustomerApplicationService(IRepository<Customer, string> customerRepository)
+        public CustomerApplicationService(IRepository<Customer, string> customerRepository, ICustomerManager customerManager)
         {
             _customerRepository = customerRepository;
+            _customerManager = customerManager;
+        }
+
+        public async Task CreateCustomerAsync(CreateCustomerDto customerDto)
+        {
+            var customer = await _customerManager.CreateAsync(customerDto.CompanyName);
+            customer.ContactName = customerDto.ContactName;
+            customer.Address = customerDto.Address;
+            customer.City = customerDto.City;
+            customer.PostalCode = customerDto.PostalCode;
+            customer.Country = customerDto.Country;
+            customer.Phone = customerDto.Phone;
+            customer.Fax = customerDto.Fax;
+            customer.Region = customerDto.Region;
+
+            await _customerRepository.InsertAsync(customer);
+        }
+
+        public async Task DeleteCustomerAsync(string customerId)
+        {
+            await _customerManager.DeleteAsync(customerId);
+        }
+
+        public async Task<CustomerDto> GetCustomerAsync(string customerId)
+        {
+            var customer = await _customerRepository.GetAsync(customerId);
+            var customerDto = ObjectMapper.Map<Customer,  CustomerDto>(customer);
+
+            return customerDto;
         }
 
         public async Task<List<CustomerDto>> GetCustomersAsync()
@@ -27,6 +58,23 @@ namespace OMSBlazor.Application.ApplicationServices
             var customerDtos = customers.Select(x => ObjectMapper.Map<Customer, CustomerDto>(x)).ToList();
 
             return customerDtos;
+        }
+
+        public async Task UpdateCustomerAsync(string id, UpdateCustomerDto customerDto)
+        {
+            var customer = await _customerRepository.GetAsync(id);
+
+            customer.SetCompanyName(customerDto.CompanyName);
+            customer.Address = customerDto.Address;
+            customer.Phone = customerDto.Phone;
+            customer.City = customerDto.City;
+            customer.ContactName = customerDto.ContactName;
+            customer.Country = customer.Country;
+            customer.Region = customer.Region;
+            customer.ContactTitle = customerDto.ContactTitle;
+            customer.Fax = customerDto.Fax;
+
+            await _customerRepository.UpdateAsync(customer);
         }
     }
 }
