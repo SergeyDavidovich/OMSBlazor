519b1f719a1451ffe9238f1033851dee03fd7aca
Bekzod Allaev <allaev_@outlook.com>
2023-11-03 22:19:59 +0200

    Create domain manager for employee

---
+++ src/OMSBlazor.Domain/DomainManagers/Emoloyee/EmployeeManager.cs
@@ -0,0 +1,32 @@
+﻿using OMSBlazor.Northwind.OrderAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Repositories;
+using Volo.Abp.Domain.Services;
+
+namespace OMSBlazor.DomainManagers.Emoloyee
+{
+    public class EmployeeManager : DomainService, IEmployeeManager
+    {
+        private readonly IRepository<Employee, int> _employeeRepository;
+
+        public EmployeeManager(IRepository<Employee, int> employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        public async Task<Employee> CreateAsync(string firstName, string lastName)
+        {
+            var lastEmployee = await _employeeRepository.LastAsync();
+
+            var id = lastEmployee.Id + 1;
+
+            var employee = new Employee(id, firstName, lastName);
+
+            return employee;
+        }
+    }
+}
---
+++ src/OMSBlazor.Domain/DomainManagers/Emoloyee/IEmployeeManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OMSBlazor.DomainManagers.Emoloyee
+{
+    public interface IEmployeeManager
+    {
+        Task<Northwind.OrderAggregate.Employee> CreateAsync(string firstName, string lastName);
+    }
+}
