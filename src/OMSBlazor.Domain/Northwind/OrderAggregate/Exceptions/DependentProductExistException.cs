13808e45a5e408c52c3f1f13ed31f5eceb8c094e
Bekzod Allaev <allaev_@outlook.com>
2023-11-05 20:20:49 +0200

    Add DependentCategoryExistException

---
+++ src/OMSBlazor.Domain/Northwind/OrderAggregate/Exceptions/DependentProductExistException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp;
+
+namespace OMSBlazor.Northwind.OrderAggregate.Exceptions
+{
+    public class DependentProductExistException : BusinessException
+    {
+        public DependentProductExistException(int categoryId, int productId)
+            : base(message: $"Category with id - {categoryId} have child product with id - {productId}")
+        {
+            
+        }
+    }
+}
