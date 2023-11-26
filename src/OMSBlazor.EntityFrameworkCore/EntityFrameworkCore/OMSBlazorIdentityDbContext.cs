a69f09c4ba13947c32e29570ff381ce0c9beb604
Bekzod Allaev <allaev_@outlook.com>
2023-11-26 21:01:24 +0200

    Revert "Update to NET 8.0 and update all NuGets"
    This reverts commit b671aa82e2f5c9e337602c09e21bed3aa183f3a6.

--- src/OMSBlazor.EntityFrameworkCore/EntityFrameworkCore/OMSBlazorIdentityDbContext.cs
+++ src/OMSBlazor.EntityFrameworkCore/EntityFrameworkCore/OMSBlazorIdentityDbContext.cs
@@ -42,8 +42,6 @@ public class OMSBlazorIdentityDbContext :
 
     public DbSet<IdentityLinkUser> LinkUsers { get; set; }
 
-    public DbSet<IdentityUserDelegation> UserDelegations { get; set; }
-
     protected override void OnModelCreating(ModelBuilder builder)
     {
         builder.SetMultiTenancySide(MultiTenancySides.Host);
