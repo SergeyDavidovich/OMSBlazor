fbcce28ec3ff349bf2a57fd704004db350baa5ff
Bekzod Allaev <allaev_@outlook.com>
2024-01-16 22:08:49 +0200

    Add migration that adds LastPasswordChangeTime in AbpUsers table

---
+++ src/OMSBlazor.EntityFrameworkCore/OMSBlazorIdentity/20240116200534_AddLastPasswordChangeTimeColumn.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OMSBlazor.OMSBlazorIdentity
+{
+    /// <inheritdoc />
+    public partial class AddLastPasswordChangeTimeColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "LastPasswordChangeTime",
+                table: "AbpUsers",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "ShouldChangePasswordOnNextLogin",
+                table: "AbpUsers",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastPasswordChangeTime",
+                table: "AbpUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ShouldChangePasswordOnNextLogin",
+                table: "AbpUsers");
+        }
+    }
+}
