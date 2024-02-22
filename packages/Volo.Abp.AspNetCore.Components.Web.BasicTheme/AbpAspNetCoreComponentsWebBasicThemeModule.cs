a7401c8fff29c67d0b7fc500c99120971ea0f4fe
Bekzod Allaev <allaev_@outlook.com>
2024-02-22 23:06:16 +0200

    Add MudBlazor to solution

--- packages/Volo.Abp.AspNetCore.Components.Web.BasicTheme/AbpAspNetCoreComponentsWebBasicThemeModule.cs
+++ packages/Volo.Abp.AspNetCore.Components.Web.BasicTheme/AbpAspNetCoreComponentsWebBasicThemeModule.cs
@@ -1,5 +1,5 @@
-﻿using Volo.Abp.AspNetCore.Components.Web.Theming;
-using Volo.Abp.AspNetCore.Components.Web.Theming.Theming;
+﻿using MudBlazor.Services;
+using Volo.Abp.AspNetCore.Components.Web.Theming;
 using Volo.Abp.Modularity;
 
 namespace Volo.Abp.AspNetCore.Components.Web.BasicTheme;
@@ -11,14 +11,7 @@ public class AbpAspNetCoreComponentsWebBasicThemeModule : AbpModule
 {
     public override void ConfigureServices(ServiceConfigurationContext context)
     {
-        Configure<AbpThemingOptions>(options =>
-        {
-            options.Themes.Add<BasicTheme>();
-
-            if (options.DefaultThemeName == null)
-            {
-                options.DefaultThemeName = BasicTheme.Name;
-            }
-        });
+        base.ConfigureServices(context);
+        context.Services.AddMudServices();
     }
 }
