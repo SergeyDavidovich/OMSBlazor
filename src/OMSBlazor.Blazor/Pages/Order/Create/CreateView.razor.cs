3f8a326a0e92b0f8421c7fedfe363a6f7e23d4f8
Bekzod Allaev <allaev_@outlook.com>
2024-03-28 18:01:06 +0200

    Add CreateView component

---
+++ src/OMSBlazor.Blazor/Pages/Order/Create/CreateView.razor.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace OMSBlazor.Blazor.Pages.Order.Create
+{
+    public partial class CreateView
+    {
+        public CreateView(CreateViewModel createViewModel)
+        {
+            ViewModel = createViewModel;
+        }
+
+        protected async override Task OnParametersSetAsync()
+        {
+            await ViewModel!.OnNavigatedTo();
+        }
+    }
+}
