c5d2f5340ab2aec8134fa672565961822e48f309
Bekzod Allaev <allaev_@outlook.com>
2024-03-31 21:06:41 +0300

    Rename hub from ProductUnistInStockUpdaterHub to ProductHub

---
+++ src/OMSBlazor.NotificationSender/Signalr/Hubs/ProductHub.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.AspNetCore.SignalR;
+
+namespace OMSBlazor.NotificationSender.Signalr.Hubs
+{
+    public class ProductHub : AbpHub
+    {
+        public async Task UpdateProductUnitsInStock(string id, int productId, int quantity)
+        {
+            Logger.LogInformation($"Sending from connection with id - {id}");
+            await Clients.All.SendAsync("UpdateQuantity", productId, quantity);
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            Logger.LogInformation($"Connected with {Context.ConnectionId}");
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            Logger.LogInformation($"Connected with {Context.ConnectionId}");
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
