271e737191aa792c4544de6ffd8307094c743e68
Bekzod Allaev <allaev_@outlook.com>
2024-04-22 22:43:21 +0300

    Redesign customer stastics with ApexCharts

--- src/OMSBlazor.Blazor/Pages/Dashboard/CustomerStastics/CustomerStasticsView.razor.cs
+++ src/OMSBlazor.Blazor/Pages/Dashboard/CustomerStastics/CustomerStasticsView.razor.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using ApexCharts;
+using System.Threading.Tasks;
 
 namespace OMSBlazor.Blazor.Pages.Dashboard.CustomerStastics
 {
@@ -11,7 +12,13 @@ namespace OMSBlazor.Blazor.Pages.Dashboard.CustomerStastics
 
         protected override async Task OnInitializedAsync()
         {
+            var legend = new Legend { Position = LegendPosition.Bottom, FontSize = "15px", HorizontalAlign = ApexCharts.Align.Center };
+            customersByCountriesOptions.Legend = legend;
+
             await ViewModel!.OnNavigatedTo();
+
+            await purchasesByCustomersChart.UpdateOptionsAsync(true, true, true);
+            await customersByCountriesChart.UpdateOptionsAsync(true, true, true);
         }
     }
 }
