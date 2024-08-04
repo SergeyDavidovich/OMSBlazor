c697cfe7a9e0ea0039a751af79101e76aa634a09
Bekzod Allaev <Allaev_@outlook.com>
2024-08-04 22:00:30 +0500

    Imporve UI

---
+++ src/OMSBlazor.Client/Themes/CustomTheme.cs
@@ -0,0 +1,20 @@
+﻿using MudBlazor;
+
+namespace OMSBlazor.Client.Themes
+{
+    public class CustomTheme : MudTheme
+    {
+        public CustomTheme()
+        {
+            LayoutProperties = new LayoutProperties()
+            {
+                DefaultBorderRadius = "3px"
+            };
+
+            PaletteDark = new PaletteDark() 
+            { 
+                
+            };
+        }
+    }
+}
