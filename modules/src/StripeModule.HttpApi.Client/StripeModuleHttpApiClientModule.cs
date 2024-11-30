fbece111950eb6ee1db255cf797b3e1aafb44886
Bekzod Allaev <Allaev_@outlook.com>
2024-11-30 20:50:43 +0100

    Add Http.Client

---
+++ modules/src/StripeModule.HttpApi.Client/StripeModuleHttpApiClientModule.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.Http.Client;
+using Volo.Abp.Modularity;
+using Volo.Abp.VirtualFileSystem;
+
+namespace StripeModule;
+
+[DependsOn(
+    typeof(StripeModuleApplicationContractsModule),
+    typeof(AbpHttpClientModule))]
+public class StripeModuleHttpApiClientModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        context.Services.AddHttpClientProxies(
+            typeof(StripeModuleApplicationContractsModule).Assembly,
+            StripeModuleRemoteServiceConsts.RemoteServiceName
+        );
+
+        Configure<AbpVirtualFileSystemOptions>(options =>
+        {
+            options.FileSets.AddEmbedded<StripeModuleHttpApiClientModule>();
+        });
+
+    }
+}
---
+++ modules/src/StripeModule.Pages/BackEndEndpointURLs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StripeModule.Pages
+{
+    public static class BackEndEndpointURLs
+    {
+        public const string Base = "api/app";
+
+        public const string CreatePayment = $"{Base}/payment/";
+    }
+}
